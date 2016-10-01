eb7bd46fd4cc7c38d98909d45fabbb545f54f9ae
Cam Jackson <camjackson89@gmail.com>
2016-10-01 20:08:37 +1000

    More stuff

--- src/components/app.js
+++ src/components/app.js
@@ -1,30 +1,26 @@
 // @flow
 import React from 'react';
+import Radium from 'radium';
 
+import { backgroundColor, center } from './styles';
 import MapPanel from './mapPanel';
 
-const navHeight = 45;
-const flexContainerStyle = { display: 'flex', justifyContent: 'flex-end', alignItems: 'center' };
+const navHeight = 55;
+const flexContainerStyle = { display: 'flex', justifyContent: 'center', alignItems: 'center' };
 const navStyle = {
   ...flexContainerStyle,
   height: navHeight,
   width: '100%',
-  backgroundColor: '#4e4a4a',
+  backgroundColor,
   position: 'fixed',
   zIndex: 10,
 };
-const flexItemStyle = { color: 'white', marginRight: 20 };
-const headingStyles = {
-  position: 'absolute',
-  color: 'white',
-  fontFamily: "'Dancing Script', cursive",
-  left: 70,
-  top: 20,
-  fontSize: '4em',
-  fontStyle: 'italic',
-};
+const linkHover = { ':hover': { color: 'white' } };
+const linkStyle = { color: '#163e3a', flex: '0 1 150px', textAlign: 'center', fontSize: 14, textDecoration: 'none', ...linkHover };
+const rsvpStyle = { ...linkStyle, color: 'white', backgroundColor: '#163e3a', padding: '10px 0px' };
+const bannerStyle = { backgroundColor, position: 'fixed', top: navHeight, zIndex: 0, width: '100%' };
 
-export default class App extends React.Component {
+class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -40,18 +36,20 @@ export default class App extends React.Component {
 
   render() {
     return (
-      <div style={{ position: 'relative' }}>
+      <div style={{ position: 'relative', backgroundColor }}>
         <nav style={navStyle}>
-          <a style={flexItemStyle}>Welcome</a>
+          <a style={linkStyle} href="a" key="Home">Home</a>
+          <a style={linkStyle} href="a" key="Our Story">Our Story</a>
+          <a style={linkStyle} href="a" key="Wedding">Wedding</a>
+          <a style={linkStyle} href="a" key="Getting">Getting There</a>
+          <a style={linkStyle} href="a" key="Gifts">Gifts</a>
+          <a style={rsvpStyle} href="a" key="RSVP">RSVP</a>
         </nav>
-        <div ref={n => { this.banner = n; }} style={{ position: 'fixed', top: 45, zIndex: 0 }}>
-          <h1 style={headingStyles}>
-            {"We're getting married!"}
-          </h1>
-          <img src="/images/orchids.jpg" alt="banner" width="100%" />
+        <div ref={n => { this.banner = n; }} style={bannerStyle}>
+          <img style={{ width: '50%', ...center, paddingBottom: 20 }} src="/images/banner.png" alt="banner" />
         </div>
         <div style={{ position: 'relative', zIndex: 5 }}>
-          <div style={{ height: this.state.bannerHeight + navHeight, visibility: 'hidden' }} />
+          <div style={{ height: this.state.bannerHeight + (navHeight), visibility: 'hidden' }} />
           <MapPanel />
         </div>
       </div>
@@ -59,3 +57,4 @@ export default class App extends React.Component {
   }
 }
 
+export default Radium(App);
---
+++ src/components/styles.js
@@ -0,0 +1,2 @@
+export const backgroundColor = '#F4C0AF';
+export const center = { marginLeft: 'auto', marginRight: 'auto', display: 'block' };
