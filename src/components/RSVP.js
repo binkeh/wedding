3bc19608694f8176b75a5f93423aacee089d91b2
Cam Jackson <camjackson89@gmail.com>
2016-11-06 21:12:01 +1100

    Beginnings of a form

--- src/components/RSVP.js
+++ src/components/RSVP.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import AnchorTarget from './AnchorTarget';
 
+// Name
+// Yes/no
+// Dietary reqs.
+
 const RSVP = () => (
   <section>
     <img className="flower" src="/flower-pink.png" alt="Pink flower" />
     <AnchorTarget id="rsvp" />
     <h1>RSVP</h1>
-    <div style={{ height: 500 }}>
-      FORM GOES HERE
-    </div>
+    <form action="">
+      <label>
+        <span>Name:</span>
+        <input type="text" name="name" />
+      </label>
+      <span>Will you be joining us?</span>
+      <label>
+        <span>{"Yes, I'll be there!"}</span>
+        <input type="radio" name="rsvp" value="yes" />
+      </label>
+      <label>
+        <span>{"Sorry, I can't make it"}</span>
+        <input type="radio" name="rsvp" value="no" />
+      </label>
+      <label htmlFor="">
+        <span>Dietary requirements:</span>
+        <textarea />
+      </label>
+      <input type="submit" />
+    </form>
   </section>
 );
 
