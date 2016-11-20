ea17c2142199844bfb459211c27058d27c63c365
binkeh <binkeh@gmail.com>
2016-11-20 17:22:49 +1100

    harro
    testing

--- src/components/Gifts.js
+++ src/components/Gifts.js
@@ -7,7 +7,7 @@ const Gifts = () => (
     <AnchorTarget id="gifts" />
     <h1>Gifts</h1>
     <p style={{ height: 500 }}>
-      As you are all travelling so far to be with us on our special day, blah blah blah.
+      As you are all travelling so far to be with us on our special day, blah blah blah. :D
     </p>
   </section>
 );
